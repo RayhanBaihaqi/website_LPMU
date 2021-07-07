d01bc4260698e40ebe8ea948bbb751f0d1c376c1
RayhanBaihaqi <baihaqir78@gmail.com>
2021-07-07 09:55:25 +0700

    tampilan login

---
+++ app/Views/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LPMU UPJ</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" href="http://localhost:8080/css/style.css">
+<body>
+	<section class="login">
+		<div class="login_box">
+			<div class="left">
+				<div class="contact">
+					<form action="">
+						<h3>LOGIN</h3>
+						<input type="text" placeholder="USERNAME">
+						<input type="text" placeholder="PASSWORD">
+						<button class="submit">LOGIN</button>
+					</form>
+				</div>
+			</div>
+			<div class="right">
+				<div class="right-text">
+					<h2>LPMU</h2>
+					<h5>LEMBAGA PENJAMINAN MUTU UNIVERSITAS</h5>
+				</div>
+				<div class="right-inductor"><img src="/img/logo_upj.png" alt=""></div>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
+
+</body>
+</html>
